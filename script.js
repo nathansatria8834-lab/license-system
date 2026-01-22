6397b765cb1f31e7cad87bb347e79115f44b0458
nathansatria8834-lab <nathansatria8834@gmail.com>
2026-01-22 23:38:13 +0700

    Add generate function to update profile fields

---
+++ script.js
@@ -0,0 +1,6 @@
+function generate() {
+  pName.innerText = name.value || "-";
+  pCall.innerText = callsign.value || "-";
+  pInst.innerText = institution.value || "-";
+  pValid.innerText = valid.value || "-";
+}
